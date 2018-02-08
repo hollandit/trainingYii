9d434556edcb43be95850ba29798172479d0e78f
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-08 17:34:39 +0300

    addded value transfer to radio

--- frontend/web/js/index.js
+++ frontend/web/js/index.js
@@ -10,4 +10,25 @@ $(document).ready(function(){
     $('.prevTestAdmin').click(function(){
         $('.test-block-admin:visible').hide().prev('.test-block-admin').show();
     });
+    $('.editQuestion').click(function(){
+        var path = $(this).data('path');
+        $('#modal-editQuestion').modal('show')
+            .find('.modal-content')
+            .load(path);
+    });
+    // $('body').on('change', '.answer-1', function () {
+    //     let value = $(this).val();
+    //     $('.radio-1').attr('value', value)
+    // })
+    valueRadio('.answer-1', '.radio-1');
+    valueRadio('.answer-2', '.radio-2');
+    valueRadio('.answer-3', '.radio-3');
+    valueRadio('.answer-4', '.radio-4');
+
+    function valueRadio(answer, radio){
+       $('body').on('change', answer, function () {
+           let value = $(this).val();
+           $(radio).attr('value', value)
+       })
+    }
 });
