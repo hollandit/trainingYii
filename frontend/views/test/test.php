06965291cf91947ba37f67fd5105e4b088d19117
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-08 17:35:34 +0300

    addded lenght array

--- frontend/views/test/test.php
+++ frontend/views/test/test.php
@@ -2,6 +2,7 @@
 
 use yii\helpers\Html;
 $write = $model[0];
+$lenght = count($model);
 ?>
 
 <div class="test-testing">
@@ -10,12 +11,12 @@ $write = $model[0];
         <?php foreach ($model as $count => $question){
             $count = $count+1;
             echo '<div class="test-block">';
-            echo 'Вопрос '.$count .'/3 <br>';
+            echo 'Вопрос '.$count .'/'.$lenght.'<br>';
             echo $question->name.'<br/>';
             foreach ($question->answear as $key => $answear){
                 echo Html::radio('Answear['.$question->id.']', false, ['label' => $answear, 'required' => true, 'value' => $answear]) . '<br/>';
             }
-            if ($count != 3){
+            if ($count != $lenght){
                 echo Html::submitButton('Ответить', ['class' => 'btn btn-primary nextTest']).'<br/>';
             } else {
                 echo Html::submitButton('Ответить', ['class' => 'btn btn-primary']).'<br/>';
