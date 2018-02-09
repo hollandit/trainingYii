0886fc4f1c66e250d616f397cc7e9ae105611c85
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-09 10:46:19 +0300

    create file

---
+++ frontend/views/test/result-test.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+
+<div class="test-resultTest">
+    <div>
+        <?php use yii\widgets\LinkPager;
+        use yii\widgets\Pjax;
+
+        Pjax::begin() ?>
+        <?php foreach ($model as $result): ?>
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <?php $user = $result->user;
+                        echo '<div>Сотрудник '.$user->last_name.' '.$user->name.' '.$user->patronymic.'</div>'; ?>
+                    <p>Тема: <?php echo $result->theme->name ?></p>
+                    <p>Дата выполнение теста <?php echo Yii::$app->formatter->asDatetime($result->date, 'dd.MM.YYYY HH:mm') ?></p>
+                    <p>Правильных ответов: <?php echo $result->result ?></p>
+                    <div class="col-lg-8">
+                        <table class="table table-condensed">
+                            <tr>
+                                <th>Вопрос</th>
+                                <th>Ответ</th>
+                            </tr>
+                            <?php foreach ($result->answear as $key => $answer){
+                                $question = \app\models\Questions::find()->where(['id' => $key])->one();
+                                echo '<tr><td>'.$question->name. '</td><td>'.$answer.'</td></tr>';
+                            } ?>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        <?php endforeach; ?>
+        <?php echo LinkPager::widget([
+                'pagination' => $pages
+        ]);
+        Pjax::end()?>
+    </div>
+</div>
