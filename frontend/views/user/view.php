d196571e02e15f65d634ed7883b229d1d56091dc
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-14 11:56:38 +0300

    added Pjax and Gridview

--- frontend/views/user/view.php
+++ frontend/views/user/view.php
@@ -1,25 +1,29 @@
 <?php
 
+use app\models\Choice;
+use yii\grid\GridView;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\widgets\DetailView;
+use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\User */
+/* @var $searchModel app\models\ChoiceSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
 
 $this->title = $model->name;
-$this->params['breadcrumbs'][] = ['label' => 'Users', 'url' => ['index']];
-$this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="user-view">
 
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('Редактировать', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Уволить', ['dismised', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => 'Вы действительно хотите уволить сотрудника?',
                 'method' => 'post',
             ],
         ]) ?>
@@ -30,18 +34,40 @@ $this->params['breadcrumbs'][] = $this->title;
         'attributes' => [
             'id',
             'username',
-            'last_name',
-            'name',
-            'patronymic',
-            'id_position',
-            'auth_key',
-            'password_hash',
-            'password_reset_token',
+            'nameEmployee',
+            [
+              'attribute' => 'id_position',
+              'value' => function($value){
+                return $value->position->name;
+              }
+            ],
             'email:email',
-            'status',
-            'created_at',
-            'updated_at',
+            'created_at:datetime',
         ],
     ]) ?>
+
+    <?php Pjax::begin() ?>
+        <?= GridView::widget([
+            'dataProvider' => $dataProvider,
+            'filterModel' => $searchModel,
+            'columns' => [
+                [
+                    'attribute' => 'id_theme',
+                    'label' => 'Темы',
+                    'filter' => ArrayHelper::map(Choice::find()->where(['id_user' => $model->id])->groupBy('id_theme')->all(), 'id_theme', 'theme.name'),
+                    'value' => function($value){
+                        return $value->theme->name;
+                    }
+                ],
+                [
+                    'attribute' => 'done',
+                    'filter' => ['0' => 'Не сдал', '1' => 'Сдал'],
+                    'value' => function($value){
+                        return $value->done == 1 ? 'Сдал' : 'Не сдал';
+                    }
+                ]
+            ]
+        ]) ?>
+    <?php Pjax::end() ?>
 
 </div>
