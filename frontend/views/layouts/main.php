4d0159c1b82633581badd6a81eed78862bc06763
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-16 17:37:29 +0300

    addeed visible menu for role

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,7 +26,7 @@ AppAsset::register($this);
 <body>
 <?php $this->beginBody() ?>
 
-<div class="wrap container">
+<div class="wrap">
 
     <?php if(!Yii::$app->user->isGuest): ?>
         <?php
@@ -34,16 +34,17 @@ AppAsset::register($this);
             'brandLabel' => 'Holland',
             'brandUrl' => Yii::$app->homeUrl,
             'options' => [
-                    'class' => 'navbar-inverse navbar-fixed-top',
+                    'class' => 'navbar-inverse header_nav',
             ],
         ]);
         $menuItems = [
-            ['label' => 'Главная страница', 'url' => ['/site/index']],
-            ['label' => 'База знание', 'url' => ['/site/knowledge']],
-            ['label' => 'Треннинги', 'url' => ['/site/contact']],
+            ['label' => 'Главная страница', 'url' => ['/site/index'], 'visible' => Yii::$app->user->can('employee')],
+            ['label' => 'База знаний', 'url' => ['/site/knowledge'], 'visible' => Yii::$app->user->can('employee')],
+            ['label' => 'Треннинги', 'url' => ['/site/contact'], 'visible' => Yii::$app->user->can('employee')],
             ['label' => 'Тесты', 'url' => ['/test/test', 'id' => 1], 'visible' => Yii::$app->user->can('hr')],
             ['label' => 'Результаты', 'url' => ['/test/result-test'], 'visible' => Yii::$app->user->can('hr')],
-            ['label' => 'Картотека', 'url' => ['user/index'], 'visible' => Yii::$app->user->can('hr')]
+            ['label' => 'Картотека', 'url' => ['user/index'], 'visible' => Yii::$app->user->can('hr')],
+            ['label' => 'База Знаний', 'url' => ['knowledge/index'], 'visible' => Yii::$app->user->can('hr')]
         ];
         if (Yii::$app->user->isGuest) {
             $menuItems[] = ['label' => 'Регистрация', 'url' => ['/site/signup']];
@@ -71,15 +72,34 @@ AppAsset::register($this);
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
         ]) ?>
         <?= Alert::widget() ?>
-        <?= $content ?>
+        <div class="container">
+            <div class="row">
+                <?= $content ?>
+            </div>
+        </div>
     </div>
 </div>
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; <?= Html::encode(Yii::$app->name) ?> <?= date('Y') ?></p>
-
-        <p class="pull-right"></p>
+        <?= Html::img('@web/images/u2367.png', ['style' => 'width:61px;margin-top:9px']) ?>
+        <div class="col-lg-3 footer-rights">
+<!--            &copy; -->
+            <?= Html::img('@web/images/u2349.png', ['style' => 'margin-top:8px;margin-right: 7px']) ?>
+            <?= date('Y') ?>
+            <span class="text-footer">All rights reserved</span>
+        </div>
+        <div class="col-lg-2"></div>
+        <div class="col-lg-3 footer-error">
+            <?= Html::img('@web/images/u2361.png') ?>
+            <?= Html::input('text', 'errors', null, ['placeholder' => 'Введите текст']).'<span>'.Html::button('ОК', ['class' => '']).'</span>' ?>
+            <div class="footer-text_errors">Нашел ошибку? Есть предложения и идеи? Напиши сюда!</div>
+        </div>
+        <div class="col-lg-3 footer-contact">
+            <?= Html::img('@web/images/u2355.png') ?>
+            <div class="phone">216-36-96</div>
+            <div class="footer-contact_text">Есть вопрос?  Позвони админу!</div>
+        </div>
     </div>
 </footer>
 
