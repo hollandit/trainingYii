5fa2c620f1c43295a49e3b2ff4d45ed0a4644db9
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-20 14:12:48 +0300

    addded post 1 in home page knowledge

--- frontend/views/site/knowledge.php
+++ frontend/views/site/knowledge.php
@@ -3,14 +3,21 @@
 use frontend\components\NavKnowledge;
 use yii\helpers\Html;
 
-$this->title = 'База знаний';;
+$this->title = 'База знаний';
+$model = $model[0];
 ?>
 
 <div class="site-index">
-    <div class="col-lg-3">
+    <div class="col-lg-3 navBar-Thema test-contant">
         <?php echo NavKnowledge::widget() ?>
     </div>
     <div class="col-lg-9">
-
+        <h2><?= Html::encode($model->title) ?></h2>
+        <?php if ($model->video != null ) {
+            echo '<iframe width="560" height="315" src='.$model->video.' frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>';
+        } else {
+            echo '';
+        } ?>
+        <div><?php echo $model->text ?></div>
     </div>
 </div>
