02e928dd2e380d5038af9430f9d9f7c60f588620
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-20 14:14:56 +0300

    remove render "_from"

--- frontend/views/user/create.php
+++ frontend/views/user/create.php
@@ -1,5 +1,8 @@
 <?php
 
+use app\models\Position;
+use yii\bootstrap\ActiveForm;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 
 
@@ -12,9 +15,31 @@ $this->title = 'Создать сотрудника';
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-        'position' => $position
-    ]) ?>
+    <div class="user-form">
+
+        <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'username')->label('Логин') ?>
+
+        <?= $form->field($model, 'last_name')->textInput(['maxlength' => true])->label('Фамилия') ?>
+
+        <?= $form->field($model, 'name')->textInput(['maxlength' => true])->label('Имя') ?>
+
+        <?= $form->field($model, 'patronymic')->textInput(['maxlength' => true])->label('Отчество') ?>
+
+        <?= $form->field($model, 'id_position')->dropDownList(ArrayHelper::map(Position::find()->all(), 'id', 'name'))->label('Должность') ?>
+
+        <?= $form->field($model, 'date_birth')->textInput(['type' => 'date']) ?>
+
+        <?= $form->field($model, 'email') ?>
+
+        <?= $form->field($model, 'password')->passwordInput()->label('Пароль') ?>
+        <div class="form-group">
+            <?= Html::submitButton('Создать', ['class' => 'btn btn-success']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+    </div>
 
 </div>
