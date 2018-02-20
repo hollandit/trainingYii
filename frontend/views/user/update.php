e7709e7b8386dcad0f021f94a188bf1b1126b8a2
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-20 14:16:13 +0300

    remove render

--- frontend/views/user/update.php
+++ frontend/views/user/update.php
@@ -1,5 +1,8 @@
 <?php
 
+use app\models\Position;
+use yii\bootstrap\ActiveForm;
+use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 
 /* @var $this yii\web\View */
@@ -11,8 +14,30 @@ $this->title = 'Редактировать сотрудника: '.$model->nameE
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+    <div class="user-form">
+
+        <?php $form = ActiveForm::begin(); ?>
+
+        <?= $form->field($model, 'username')->label('Логин') ?>
+
+        <?= $form->field($model, 'last_name')->textInput(['maxlength' => true])->label('Фамилия') ?>
+
+        <?= $form->field($model, 'name')->textInput(['maxlength' => true])->label('Имя') ?>
+
+        <?= $form->field($model, 'patronymic')->textInput(['maxlength' => true])->label('Отчество') ?>
+
+        <?= $form->field($model, 'id_position')->dropDownList(ArrayHelper::map(Position::find()->all(), 'id', 'name'))->label('Должность') ?>
+
+        <?= $form->field($model, 'date_birth')->textInput(['type' => 'date']) ?>
+
+        <?= $form->field($model, 'email') ?>
+
+        <div class="form-group">
+            <?= Html::submitButton('Редактировать', ['class' => 'btn btn-success']) ?>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+    </div>
 
 </div>
