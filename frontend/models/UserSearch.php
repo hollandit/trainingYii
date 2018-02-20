42f3f8c149c68a4e5fb788a1cad4a8d6f1d32026
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-20 14:11:53 +0300

    added attribute $date_to and $date_from. Remove attribute created_at

--- frontend/models/UserSearch.php
+++ frontend/models/UserSearch.php
@@ -2,25 +2,25 @@
 
 namespace app\models;
 
-use Yii;
 use yii\base\Model;
 use yii\data\ActiveDataProvider;
-use app\models\User;
-
 /**
  * UserSearch represents the model behind the search form of `app\models\User`.
  */
 class UserSearch extends User
 {
     public $nameEmployee;
+    public $date_from;
+    public $date_to;
     /**
      * @inheritdoc
      */
     public function rules()
     {
         return [
-            [['id', 'id_position', 'status', 'created_at', 'updated_at'], 'integer'],
+            [['id', 'id_position', 'status'], 'integer'],
             [['username', 'last_name', 'name', 'patronymic', 'nameEmployee','auth_key', 'password_hash', 'password_reset_token', 'email', 'date_birth'], 'safe'],
+            [['date_from', 'date_to'], 'date', 'format' => 'php:d.m.Y']
         ];
     }
 
@@ -50,6 +50,21 @@ class UserSearch extends User
             'query' => $query,
         ]);
 
+        $dataProvider->setSort([
+            'attributes' => [
+                'id',
+                'username',
+                'date_birth',
+                'id_position',
+                'created_at',
+                'nameEmployee' => [
+                    'asc' => ['last_name' =>SORT_ASC, 'name' => SORT_ASC, 'patronymic' => SORT_ASC],
+                    'desc' => ['last_name' =>SORT_DESC, 'name' => SORT_DESC, 'patronymic' => SORT_DESC],
+                    'default' => SORT_ASC
+                ]
+            ]
+        ]);
+
         $this->load($params);
 
         if (!$this->validate()) {
@@ -64,19 +79,16 @@ class UserSearch extends User
             'id_position' => $this->id_position,
             'date_birth' => $this->date_birth,
             'status' => $this->status,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
         ]);
 
         $query->andFilterWhere(['like', 'username', $this->username])
             ->andFilterWhere(['like', 'last_name', $this->nameEmployee])
-            ->andFilterWhere(['like', 'name', $this->nameEmployee])
-            ->andFilterWhere(['like', 'patronymic', $this->nameEmployee])
-            ->andFilterWhere(['like', 'auth_key', $this->auth_key])
-            ->andFilterWhere(['like', 'password_hash', $this->password_hash])
-            ->andFilterWhere(['like', 'password_reset_token', $this->password_reset_token])
+            ->orFilterWhere(['like', 'name', $this->nameEmployee])
+            ->orFilterWhere(['like', 'patronymic', $this->nameEmployee])
             ->andFilterWhere(['like', 'email', $this->email])
-            ->andFilterWhere(['like', 'date_birth', $this->date_birth]);
+            ->andFilterWhere(['like', 'date_birth', $this->date_birth])
+            ->andFilterWhere(['>=', 'created_at', $this->date_from ? strtotime($this->date_from.' 00:00:00') : null])
+            ->andFilterWhere(['<=', 'created_at', $this->date_to ? strtotime($this->date_to.' 23:59:59') : null]);
 
         return $dataProvider;
     }
