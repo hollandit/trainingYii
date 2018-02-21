5fcccd53365f45842a23ac29c467b55085011147
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-21 13:15:28 +0300

    put php code

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -3,23 +3,27 @@
 use \yii\helpers\Html;
 
 /* @var $this yii\web\View */
+/* @var $user \app\models\User */
+/** @var $thema \app\models\Thema */
+/** @var  $count \app\models\Choice */
+/** @var  $done \app\models\Choice */
+/** @var  $failed \app\models\Choice */
 
 $this->title = 'Главная страница';
 ?>
 <div class="site-index">
-
-    <h3>Добро пожаловать <?php echo $user->last_name.' '.$user->name.' '.$user->patronymic ?></h3>
-    <h4>Должность: <?php echo $user->position->name ?></h4>
-
-    <h3>Доступные тесты</h3>
-        <?php foreach ($thema as $name){
-            echo Html::a($name->name, ['test/testing', 'id' => $name->id]).'<br>';
-        } ?>
-        <?= Html::a('Все тесты', ['#'], ['class' => 'btn btn-link']) ?>
-    <h3>Доступные тренинги</h3>
-
-    <h3>Статистика</h3>
-    <p>Количество тестов: 0</p>
-    <p>Пройденные тесты: 0</p>
-    <p>Проваленные тесты: 0</p>
+    <div class="col-lg-5">
+        <h3>Добро пожаловать <?php echo $user->nameEmployee ?></h3>
+        <h4>Должность: <?php echo $user->position->name ?></h4>
+        <h3>Доступные тесты</h3>
+            <?php foreach ($thema as $name){
+                echo Html::a($name->name, ['test/testing', 'id' => $name->id]).'<br>';
+            } ?>
+<!--            --><?//= Html::a('Все тесты', ['#'], ['class' => 'btn btn-link']) ?>
+        <h3>Доступные тренинги</h3>
+        <h3>Статистика</h3>
+        <p>Количество тестов: <?= $count ?></p>
+        <p>Пройденные тесты: <?= $done ?></p>
+        <p>Проваленные тесты: <?= $failed ?></p>
+    </div>
 </div>
