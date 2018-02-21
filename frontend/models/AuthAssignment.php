ae13e53d733ecce9fcf8481104e6186b7427a060
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-21 11:35:06 +0300

    attribute user_id should be integer

--- frontend/models/AuthAssignment.php
+++ frontend/models/AuthAssignment.php
@@ -30,8 +30,8 @@ class AuthAssignment extends \yii\db\ActiveRecord
     {
         return [
             [['item_name', 'user_id'], 'required'],
-            [['created_at'], 'integer'],
-            [['item_name', 'user_id'], 'string', 'max' => 64],
+            [['created_at', 'user_id'], 'integer'],
+            [['item_name'], 'string', 'max' => 64],
             [['item_name', 'user_id'], 'unique', 'targetAttribute' => ['item_name', 'user_id']],
             [['item_name'], 'exist', 'skipOnError' => true, 'targetClass' => AuthItem::className(), 'targetAttribute' => ['item_name' => 'name']],
         ];
@@ -55,14 +55,5 @@ class AuthAssignment extends \yii\db\ActiveRecord
     public function getItemName()
     {
         return $this->hasOne(AuthItem::className(), ['name' => 'item_name']);
-    }
-
-    /**
-     * @inheritdoc
-     * @return \app\models\query\AuthAssignmentQuery the active query used by this AR class.
-     */
-    public static function find()
-    {
-        return new \app\models\query\AuthAssignmentQuery(get_called_class());
     }
 }
