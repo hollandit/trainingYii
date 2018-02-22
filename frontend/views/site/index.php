57137e499c8fd8fd2de8631ead0b29b564e06eb7
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-22 17:45:49 +0300

    moved block information

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -10,17 +10,24 @@ use \yii\helpers\Html;
 /** @var  $failed \app\models\Choice */
 
 $this->title = 'Главная страница';
+
 ?>
 <div class="site-index">
     <div class="col-lg-5">
-        <h3>Добро пожаловать <?php echo $user->nameEmployee ?></h3>
-        <h4>Должность: <?php echo $user->position->name ?></h4>
+        <h3>Добро пожаловать <br><?= $user->nameEmployee ?></h3>
+        <h4>Должность: <?= $user->position->name ?></h4>
+        <h4>Базовый оклад: <?= $user->salary.' рублей' ?></h4>
+    </div>
+    <div class="col-lg-5">
         <h3>Доступные тесты</h3>
-            <?php foreach ($thema as $name){
-                echo Html::a($name->name, ['test/testing', 'id' => $name->id]).'<br>';
-            } ?>
-<!--            --><?//= Html::a('Все тесты', ['#'], ['class' => 'btn btn-link']) ?>
+        <?php foreach ($thema as $name){
+            echo Html::a($name->name, ['test/testing', 'id' => $name->id]).'<br>';
+        } ?>
+        <?= Html::a('Все тесты', ['#'], ['class' => 'btn btn-link']) ?>
         <h3>Доступные тренинги</h3>
+        <?= Html::a('Все тренинги', ['#'], ['class' => 'btn btn-link']) ?>
+    </div>
+    <div class="col-lg-2">
         <h3>Статистика</h3>
         <p>Количество тестов: <?= $count ?></p>
         <p>Пройденные тесты: <?= $done ?></p>
