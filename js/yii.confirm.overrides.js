32d1dbd8dcdadc90fcd2e28fb4d99aa2b24dda65
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-02-27 09:23:52 +0300

    create file

---
+++ js/yii.confirm.overrides.js
@@ -0,0 +1,17 @@
+/**
+ * Override the default yii confirm dialog. This function is
+ * called by yii when a confirmation is requested.
+ *
+ * @param message
+ * @param okCallback
+ * @param cancelCallback
+ */
+yii.confirm = function (message, okCallback, cancelCallback) {
+    swal({
+        title: message,
+        // type: 'warning',
+        showCancelButton: true,
+        closeOnConfirm: true,
+        allowOutsideClick: true
+    }, okCallback);
+};
