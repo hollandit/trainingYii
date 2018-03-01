c625f53b1e0fae2bac584e7bcd9fa1783f977873
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-03-01 17:42:34 +0300

    send form and output

--- frontend/web/js/index.js
+++ frontend/web/js/index.js
@@ -21,9 +21,48 @@ $(document).ready(function(){
         $(this).find('span:last').remove();
     });
 
+    $('.test-answer').click(function () {
+        $('.answerButton').trigger('click');
+    });
+
+    //Form test
+    $('#testForm-result').submit(function(e){
+        e.preventDefault();
+        let id = $(this).data('id');
+        let form = $(this).serialize();
+        $.post({
+            url: 'http://hosttraining/frontend/web/index.php?r=test%2Ftesting&id='+id,
+            data: form,
+        })
+            .done(data => {
+                let result = JSON.parse(data);
+                $('#modalResult').modal('show');
+                $('#modalResult').on('hide.bs.modal', function () {
+                    window.location.replace('http://hosttraining/frontend/web/index.php?r=site%2Findex');
+                });
+                if (result.status === 1){
+                    clearInterval(intervalID);
+                    $('.modal-result_test').html('<div class="modal-result_title">Поздравляем Вас с успешным прохождением теста!<br/>' +
+                        'Количество допустимых ошибок: 2.</div><div class="modal-result_information">Это неплохой результат.<br>' +
+                        'Этот тест Вы сможете пройти не раньше, чем через 2 недели.<br>' +
+                        'Однако, Вы можете запросить более сложный.</div><div class="modal-result_information">' +
+                        'Дерзайте!' +
+                        '</div>')
+                } else {
+                    clearInterval(intervalID);
+                    $('.modal-result_test').html('<div class="modal-result_title">К сожалению, Вы не прошли тест.<br/>' +
+                        '</div><div class="modal-result_information">Это плохой результат.<br>' +
+                        'Этот тест Вы сможете пройти не раньше, чем через 2 недели.<br>' +
+                        'Рекомендую Вам не тратить это время зря и основательно подготовиться.</div><div class="modal-result_information">' +
+                        'Успехов!' +
+                        '</div>')
+                }
+            })
+            .fail(err => console.log(err.responseText));
+    });
+
     //FileImage
     $('body').on('change', '.test-created-input', function(){
-        console.log('Hello world');
         $('.test-created-span').remove();
         uploadFile(this, '.test-created-image');
     });
@@ -32,7 +71,6 @@ $(document).ready(function(){
     $('body').on('click', '.buttonNavDelete',function (e) {
         e.preventDefault();
         let id = $(this).data('id');
-        console.log(id);
         let div = document.createElement('div');
         div.className = 'delete-modal';
         div.onclick = function (e) {
@@ -43,6 +81,21 @@ $(document).ready(function(){
             }
         };
         div.innerHTML='<div><h1>Уверены?...</h1><br/><p>Это действие необратим. Если Вы сейчас удалите тест, то уже не сможете его восстановить.</p></div><input type="button" value="Отменить"><input type="button" class="delete-button" value="Да, уверен">';
+        return document.body.appendChild(div);
+    });
+
+    $('#completion-button').click(function (e) {
+        e.preventDefault();
+        let div = document.createElement('div');
+        div.className = 'completion-modal';
+        div.onclick = function (e) {
+            let t = e ? e.target : window.event.srcElement;
+            if(t.tagName === 'INPUT'){
+                t.value === 'Завершить' && window.location.replace('http://hosttraining/frontend/web/index.php?r=site%2Findex');
+                this.parentNode.removeChild(this);
+            }
+        };
+        div.innerHTML='<div><h1>Уверены?...</h1><br/><p>Если Вы завершите тест сейчас, то он будет считаться непройденным. Это отрицательно отразится на Вашем рейтинге.</p></div><input type="button" value="Вернуться к тесту"><input type="button" class="completion-button" value="Завершить">';
         return document.body.appendChild(div);
     });
 
