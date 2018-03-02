9e046395af2d3625926b5651ecdcea9b8cee7f84
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-03-02 17:36:54 +0300

    added block create time testing

--- frontend/views/test/update-theme.php
+++ frontend/views/test/update-theme.php
@@ -10,6 +10,19 @@ use yii\widgets\ActiveForm;
     ]) ?>
 
     <?= $form->field($model, 'name')->textInput() ?>
+    <div class="create-test_control">
+        <hr>
+        <div class="col-lg-8">
+            <p>Укажите количество времени для теста:</p>
+        </div>
+        <div class="col-lg-4 update-time">
+            <?=
+            $form->field($model, 'minute')->textInput(['type' => 'number', 'class' => 'create-test_timeInput', 'min' => 0, 'max' => '59'])->label(false).
+            Html::input('text', null, ':', ['class' => 'control-operator', 'disabled' => true]).
+            $form->field($model, 'second')->textInput(['type' => 'number', 'class' => 'create-test_timeInput', 'min' => '0', 'max' => '59'])->label(false) ?>
+        </div>
+        <hr>
+    </div>
 
     <div class="form-group">
         <?= Html::submitButton('Сохранить', ['class' => 'btn btn-primary']) ?>
