272c2dced625c1e61ad52ccd5c5509fb7e9e1503
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-03-06 17:02:56 +0300

    added kladr widget and phone mask

--- frontend/views/user/create.php
+++ frontend/views/user/create.php
@@ -1,9 +1,11 @@
 <?php
 
+use andkon\yii2kladr\Kladr;
 use app\models\Position;
 use yii\bootstrap\ActiveForm;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
+use yii\widgets\MaskedInput;
 
 
 /* @var $this yii\web\View */
@@ -27,18 +29,43 @@ $this->title = 'Создать сотрудника';
 
             <?= $form->field($model, 'patronymic')->textInput(['maxlength' => true, 'placeholder' => 'Отчество'])->label(false) ?>
 
-<!--            --><?//= $form->field($model, 'city')->textInput(['maxlength' => true, 'placeholder' => 'Город'])->label(false) ?>
-            <?= $form->field($model, 'city')->widget(\andkon\yii2kladr\Kladr::className(), [
-                'type' => \andkon\yii2kladr\Kladr::TYPE_CITY,
+            <?= $form->field($model, 'phone')->widget(MaskedInput::className(), [
+                'mask' => '8(999)999-99-99',
                 'options' => [
-                    'placeholder' => 'Город',
-                    'class' => 'form_input'
+                    'placeholder' => 'Телефон',
+                    'class' => 'form-control'
                 ]
             ])->label(false) ?>
 
-            <?= $form->field($model, 'street')->textInput(['maxlength' => true, 'placeholder' => 'Улица'])->label(false) ?>
+            <?= $form->field($model, 'city')->widget(Kladr::className(), [
+                'type' => Kladr::TYPE_CITY,
+                'options' => [
+                    'placeholder' => 'Город',
+                    'class' => 'form-control'
+                ]
+            ])->label(false) ?>
 
-            <?= $form->field($model, 'build')->textInput(['maxlength' => true, 'placeholder' => 'Дом'])->label(false) ?>
+            <?= Html::input('text', 'city_name', '', ['class' => 'city-name']) ?>
+
+            <?= $form->field($model, 'street')->widget(Kladr::className(), [
+                'type' => Kladr::TYPE_STREET,
+                'options' => [
+                    'placeholder' => 'Улица',
+                    'class' => 'form-control'
+                ]
+            ])->label(false) ?>
+
+            <?= Html::input('text', 'street_name', '', ['class' => 'street-name']) ?>
+
+            <?= $form->field($model, 'build')->widget(Kladr::className(), [
+                'type' => Kladr::TYPE_BUILDING,
+                'options' => [
+                    'placeholder' => 'Дом',
+                    'class' => 'form-control'
+                ]
+            ])->label(false) ?>
+
+            <?= Html::input('text', 'build_name', '', ['class' => 'build-name']) ?>
 
             <?= $form->field($model, 'appartament')->textInput(['maxlength' => true, 'placeholder' => 'Квартира'])->label(false) ?>
 
@@ -52,11 +79,13 @@ $this->title = 'Создать сотрудника';
                         'required' => true
                     ])->label(false) ?>
 
+            <?= $form->field($model, 'salary')->textInput(['type' => 'number', 'placeholder' => 'Оклад'])->label(false) ?>
+
             <?= $form->field($model, 'date_birth')->textInput(['type' => 'date', 'placeholder' => 'Дата рождение'])->label(false) ?>
 
-            <?= $form->field($model, 'email')->textInput(['placeholder' => 'Email'])->label(false) ?>
+            <?= $form->field($model, 'email')->textInput(['type' => 'email','placeholder' => 'Email'])->label(false) ?>
 
-            <?= $form->field($model, 'password')->passwordInput(['placeholder' => 'Пароль'])->label(false) ?>
+            <?= $form->field($model, 'password')->passwordInput(['type' => 'password','placeholder' => 'Пароль'])->label(false) ?>
             <div class="form-group">
                 <?= Html::submitButton('Создать', ['class' => 'btn btn-success']) ?>
             </div>
