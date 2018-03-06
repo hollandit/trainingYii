5043a42050b209529078050d4651b2f95ae6486c
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-03-06 17:04:42 +0300

    commit dismissal employee and widgets placed in try/catch

--- frontend/views/user/index.php
+++ frontend/views/user/index.php
@@ -2,11 +2,9 @@
 
 use kartik\date\DatePicker;
 use kartik\datecontrol\DateControl;
-use yii\bootstrap\Modal;
 use yii\helpers\ArrayHelper;
 use yii\helpers\Html;
 use yii\grid\GridView;
-use yii\widgets\ActiveForm;
 use yii\widgets\Pjax;
 
 /* @var $this yii\web\View */
@@ -24,43 +22,44 @@ $this->title = 'Картотека';
 
     <p>
         <?= Html::a('Создать сотрудника', ['create'], ['class' => 'btn btn-success']) ?>
-        <?php Modal::begin([
-            'header' => 'Создать должность',
-            'toggleButton' => [
-                'tag' => 'button',
-                'class' => 'btn btn-primary',
-                'label' => 'Создать должность'
-            ]
-        ]);
-
-        $form = ActiveForm::begin([
-            'id' => 'user-create-position',
-            'action' => ['create-position']
-        ]);
-
-        echo $form->field($position, 'name')->textInput();
-
-        echo Html::submitButton('Создать', ['class' => 'tbn btn-primary']);
-
-        ActiveForm::end();
-
-        Modal::end(); ?>
+<!--        --><?php //Modal::begin([
+//            'header' => 'Создать должность',
+//            'toggleButton' => [
+//                'tag' => 'button',
+//                'class' => 'btn btn-primary',
+//                'label' => 'Создать должность'
+//            ]
+//        ]);
+//
+//        $form = ActiveForm::begin([
+//            'id' => 'user-create-position',
+//            'action' => ['create-position']
+//        ]);
+//
+//        echo $form->field($position, 'name')->textInput();
+//
+//        echo Html::submitButton('Создать', ['class' => 'tbn btn-primary']);
+//
+//        ActiveForm::end();
+//
+//        Modal::end(); ?>
     </p>
 
     <?php Pjax::begin() ?>
-        <?= GridView::widget([
-            'dataProvider' => $dataProvider,
-            'filterModel' => $searchModel,
-            'columns' => [
-                'id',
-                'username',
-                [
-                    'attribute' => 'nameEmployee',
-                    'label' => 'ФИО'
-                ],
-                [
-                    'attribute' => 'date_birth',
-                    'format' => 'date',
+        <?php try {
+            echo GridView::widget([
+                'dataProvider' => $dataProvider,
+                'filterModel' => $searchModel,
+                'columns' => [
+                    'id',
+                    'username',
+                    [
+                        'attribute' => 'nameEmployee',
+                        'label' => 'ФИО'
+                    ],
+                    [
+                        'attribute' => 'date_birth',
+                        'format' => 'date',
                         'filter' => DateControl::widget([
                             'model' => $searchModel,
                             'attribute' => 'date_birth',
@@ -68,32 +67,35 @@ $this->title = 'Картотека';
                             'type' => DateControl::FORMAT_DATE,
                             'displayFormat' => 'dd.MM.yyyy'
                         ])
+                    ],
+                    [
+                        'attribute' => 'id_position',
+                        'filter' => ArrayHelper::map(\app\models\Position::find()->all(), 'id', 'name'),
+                        'value' => function ($value) {
+                            return $value->position->name;
+                        },
+                        'label' => 'Должность'
+                    ],
+                    [
+                        'filter' => DatePicker::widget([
+                            'model' => $searchModel,
+                            'language' => 'ru',
+                            'attribute' => 'date_from',
+                            'attribute2' => 'date_to',
+                            'type' => DatePicker::TYPE_RANGE,
+                            'separator' => '-',
+                            'pluginOptions' => ['format' => 'dd.mm.yyyy'],
+                        ]),
+                        'attribute' => 'created_at',
+                        'format' => 'datetime',
+                        'label' => 'Дата приема'
+                    ],
+
+                    ['class' => 'yii\grid\ActionColumn'],
                 ],
-                [
-                    'attribute' => 'id_position',
-                    'filter' => ArrayHelper::map(\app\models\Position::find()->all(), 'id', 'name'),
-                    'value' => function($value){
-                        return $value->position->name;
-                    },
-                    'label' => 'Должность'
-                ],
-                [
-                    'filter' => DatePicker::widget([
-                        'model' => $searchModel,
-                        'language' => 'ru',
-                        'attribute' => 'date_from',
-                        'attribute2' => 'date_to',
-                        'type' => DatePicker::TYPE_RANGE,
-                        'separator' => '-',
-                        'pluginOptions' => ['format' => 'dd.mm.yyyy'],
-                    ]),
-                    'attribute' => 'created_at',
-                    'format' => 'datetime',
-                    'label' => 'Дата приема'
-                ],
-    
-                ['class' => 'yii\grid\ActionColumn'],
-            ],
-        ]); ?>
+            ]);
+        } catch (Exception $e) {
+            print_r($e->getMessage());
+        } ?>
     <?php Pjax::end() ?>
 </div>
