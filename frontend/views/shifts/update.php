1d18528373039c4778f06154e37ead94b49f39ed
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-03-26 14:05:50 +0300

    remove breadcrumbs

--- frontend/views/shifts/update.php
+++ frontend/views/shifts/update.php
@@ -5,14 +5,8 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model app\models\Shifts */
 
-$this->title = 'Update Shifts: {nameAttribute}';
-$this->params['breadcrumbs'][] = ['label' => 'Shifts', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' => $model->id]];
-$this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="shifts-update">
-
-    <h1><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', compact('model', 'user')) ?>
 
