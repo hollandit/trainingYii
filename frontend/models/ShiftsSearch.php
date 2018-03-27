bd1d768321f9fed41c54caccf1a2de1f573f5012
Ruslan Absalyamov <absaruslan9@yandex.ru>
2018-03-27 17:22:17 +0300

    seacrh shifts Model

--- frontend/models/ShiftsSearch.php
+++ frontend/models/ShiftsSearch.php
@@ -2,71 +2,56 @@
 
 namespace app\models;
 
+use DateTimeImmutable;
 use Yii;
-use yii\base\Model;
-use yii\data\ActiveDataProvider;
-use app\models\Shifts;
 
 /**
  * ShiftsSearch represents the model behind the search form of `app\models\Shifts`.
  */
 class ShiftsSearch extends Shifts
 {
-    /**
-     * @inheritdoc
-     */
     public function rules()
     {
         return [
-            [['id', 'user_id'], 'integer'],
-            [['start_date', 'end_date', 'created_at', 'updated_at'], 'safe'],
+            [['shop_id'], 'integer'],
+            ['date', 'safe']
         ];
     }
 
     /**
-     * @inheritdoc
+     * @return object
      */
-    public function scenarios()
+    public function search()
     {
-        // bypass scenarios() implementation in the parent class
-        return Model::scenarios();
-    }
+        $request = Yii::$app->request;
+        $shifts = [];
+        $shop = $request->get('ShiftsSearch')['shop_id'];
 
-    /**
-     * Creates data provider instance with search query applied
-     *
-     * @param array $params
-     *
-     * @return ActiveDataProvider
-     */
-    public function search($params)
-    {
-        $query = Shifts::find();
+        $date = $request->get('ShiftsSearch')['date'] == null
+            ? date('W')
+            : preg_split('/-W/', $request->get('ShiftsSearch')['date'])[1];
+        $date_start = new DateTimeImmutable($date.' week first monday of Jan this year - 1 week');
+        $date_end = $date_start->modify('+6 days');
 
-        // add conditions that should always apply here
-
-        $dataProvider = new ActiveDataProvider([
-            'query' => $query,
-        ]);
-
-        $this->load($params);
-
-        if (!$this->validate()) {
-            // uncomment the following line if you do not want to return any records when validation fails
-            // $query->where('0=1');
-            return $dataProvider;
+        $weekDay = [
+            'Пн' => $date_start->format('Y-m-d'),
+            'Вт' => $date_start->modify('+1 days')->format('Y-m-d'),
+            'Ср' => $date_start->modify('+2 days')->format('Y-m-d'),
+            'Чт' => $date_start->modify('+3 days')->format('Y-m-d'),
+            'Пт' => $date_start->modify('+4 days')->format('Y-m-d'),
+            'Сб' => $date_start->modify('+5 days')->format('Y-m-d'),
+            'Вс' => $date_end->format('Y-m-d'),
+        ];
+        $query = Shifts::find()->with('shop')->andWhere(['between', 'date', $date_start->format('Y-m-d'), $date_end->format('Y-m-d')]);
+        $shops = $shop == null ? Shop::find()->all() : Shop::findOne($shop);
+        if ($request->get('ShiftsSearch')['shop_id'] == null){
+            foreach ($shops as $shop){
+                $shifts[$shop->name] = Shifts::find()->with('shop')->andWhere(['between', 'date', $date_start->format('Y-m-d'), $date_end->format('Y-m-d')])->andWhere(['shop_id' => $shop->id])->all();
+            }
+        } else {
+            $shifts[$shops->name] = Shifts::find()->with('shop')->andWhere(['between', 'date', $date_start->format('Y-m-d'), $date_end->format('Y-m-d')])->andWhere(['shop_id' => $shop])->all();
         }
 
-        // grid filtering conditions
-        $query->andFilterWhere([
-            'id' => $this->id,
-            'user_id' => $this->user_id,
-            'start_date' => $this->start_date,
-            'end_date' => $this->end_date,
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
-        ]);
-
-        return $dataProvider;
+        return (object)compact('query', 'weekDay', 'shifts');
     }
 }
